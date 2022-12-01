a9610f369022b8072df29c9e2801a76952ae2745
Reginald Graham <reg.graham98@gmail.com>
2022-12-01 09:59:30 -0600

    working on function-drills

--- git@github.com:ReginaldGraham/function-drill.js
+++ git@github.com:ReginaldGraham/function-drill.js
@@ -183,80 +183,80 @@
 // }
 // console.log(cube(3))
 // Write a function named squareRoot(n) that returns the square root of the input
-function squareRoot(number) {
-    var number;
-    for (var i = number; i >= 1; i--) {
-        if (i * i === number) {
-            number = i;
-            break;
-        }
-    }
-    return number;
-}
-console.log(squareRoot(9))
+// function squareRoot(number) {
+//     var number;
+//     for (var i = number; i >= 1; i--) {
+//         if (i * i === number) {
+//             number = i;
+//             break;
+//         }
+//     }
+//     return number;
+// }
+// console.log(squareRoot(9))
 // Write a function named cubeRoot(n) that returns the cube root of the input
-function cubeRoot(number) {
-    var number;
-    for (var i = number; i >= 1; i--) {
-        if (i * i * i === number) {
-            number = i;
-            break;
-        }
-    }
-    return number;
-}
-console.log(cubeRoot(64))
+// function cubeRoot(number) {
+//     var number;
+//     for (var i = number; i >= 1; i--) {
+//         if (i * i * i === number) {
+//             number = i;
+//             break;
+//         }
+//     }
+//     return number;
+// }
+// console.log(cubeRoot(64))
 // Write a function named invertSign(number) that returns a negative version of a postive number, a
 // positve version of negative, and false for all else.
-function invertSign(number){
-    return number * -1
-}
-console.log(invertSign(-2))
+// function invertSign(number){
+//     return number * -1
+// }
+// console.log(invertSign(-2))
 // Write a function named degreesToRadians(number)
-function degreeToRadians(input){
-    return input*3.14/180
-}
+// function degreeToRadians(input){
+//     return input*3.14/180
+// }
 
 // 1) Make a function called returnTwo() that returns the number 2 when called
 // ---Test this function with console.log(returnTwo())
 
-function returnTwo(){
-    return 2
-}
-console.log(returnTwo())
+// function returnTwo(){
+//     return 2
+// }
+// console.log(returnTwo())
 
 // 2) Make a function called returnName() that returns the string of your name
 // ---Test this function with console.log(returnName())
-function returnName(input){
-    return input;
-}
-console.log(returnName(`reginald`))
-
+// function returnName(input){
+//     return input;
+// }
+// console.log(returnName(`reginald`))
+//
 
 // 3) Make a function called addThree() which takes in a number input and returns the number plus 3.
 // ---Test this function with console.log(addThree(5))
-function addThree(number){
-    return number+3
-}
-console.log(addThree(5))
+// function addThree(number){
+//     return number+3
+// }
+// console.log(addThree(5))
 
 // 4) Make a function called sayString() which returns the string input passed in.
 // ---Test this function with console.log(sayString())
-function sayString(string){
-    return string.toString()
-}
-console.log(sayString(`t`))
-
-
+// function sayString(string){
+//     return string.toString()
+// }
+// console.log(sayString(`t`))
+//
+//
 
 // 5) Make a function called sayHowdy() which console.logs the string "Howdy!"
 // ---Test this function by directly calling sayHowdy()
 // ---Remember this function does not need a defined return value
 
-function sayHowdy(){
-    return `Howdy!`
-}
-console.log(sayHowdy())
+// function sayHowdy(){
+//     return `Howdy!`
+// }
+// console.log(sayHowdy())
 
 // write a function, abbrevToDay, that takes in a three-character abbreviation for the day of the week and returns the complete day of the week
 // Example: abbrevToDay('mon') returns 'Monday'
@@ -285,4 +285,45 @@ function abbrevToDay(input){
     return input
 }
 
-console.log(abbrevToDay(`Fri`))
+console.log(abbrevToDay(`frfr`))
+// Write a function called identity(input) that takes in an argument called input and returns that input.
+
+function identity(input){
+    return input
+}
+console.log(identity(`hey`))
+
+
+//     Write a function called getRandomNumber(min, max) that returns a random number between min and max values sent to that function call.
+
+function getRandomNumber(x,y){
+    return Math.floor(Math.random()*y)
+}
+console.log(getRandomNumber(5,10))
+// Write a function called first(input) that returns the first character in the provided string.
+//
+//     Write a function called last(input) that returns the last character of a string
+//
+// Write a function called rest(input) that returns everything but the first character of a string.
+//
+//     Write a function called reverse(input) that takes a string and returns it reversed.
+//
+//     Write a function called isNumeric(input) that takes an input and returns a boolean if the input is numeric.
+//
+//     Write a function called count(input) that takes in a string and returns the number of characters.
+//
+//     Write a function called add(a, b) that returns the sum of a and b
+//
+// Write a function called subtract(a, b) that return the difference between the two inputs.
+//
+//     Write multiply(a, b) function that returns the product
+//
+// Write a divide(numerator, denominator) function that returns a divided by b
+//
+// Write a remainder(number, divisor) function that returns the remainder left over when dividing number by the divisor
+//
+// Write the function square(a) that takes in a number and returns the number multiplied by itself.
+//
+//     Write a function called sumOfSquares(a, b) that uses only your add() function and your square function and not + or * operators
+//
+// Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
