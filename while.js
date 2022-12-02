5054b543025896199ae3cc02ea1257dbcfa6b20b
Reginald Graham <reg.graham98@gmail.com>
2022-12-02 10:26:13 -0600

    working on function-drills

--- while.js
+++ while.js
@@ -4,6 +4,17 @@
 //     console.log('while loop iteration #' + i);
 //     i*=2;
 // }
+// let numMultiplyBy=2;
+// let counter=0;
+// while(counter<16){
+//     console.log(numMultiplyBy);
+//     numMultiplyBy*=2;
+//     counter++;
+// }
+// for (let i=0, num=2;i<16; i++){
+//     console.log(num);
+//     num*=2;
+// }
 // function iceCream() {
 //     let stock = Math.round(Math.random() * (100 - 50) + 50);
 //     console.log(stock)
@@ -30,3 +41,21 @@
 //
 // }
 // iceCream();
+// let conesToSell=Math.floor(Math.random() * 50) + 50
+// do{
+//     let conesToBuy=Math.floor(Math.random() * 5) + 1;
+//     if(conesToBuy <=conesToSell){
+//         console.log(`${conesToBuy} cones sold...`);
+//         conesToSell-= conesToBuy;
+//     }else{
+//         console.log(`Cannot sell you ${conesToBuy} , I only have ${conesToSell}`);
+//         console.log(`${conesToSell} cones sold...`);
+//         conesToSell-=conesToSell;
+//     }
+//     if(conesToSell===0){
+//         console.log('i sold them all');
+//         break;
+//
+//     }
+//
+// }while (conesToSell>0)
