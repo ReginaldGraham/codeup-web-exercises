cad8fffb9c297c3df78bfb740bfbf98b7f711e44
Reginald Graham <reg.graham98@gmail.com>
2022-12-05 14:22:09 -0600

    working on function-drills

--- planets-string.js
+++ planets-string.js
@@ -1,6 +1,55 @@
 (function(){
     "use strict";
 
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    // console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift(`The sun`);
+    console.log(planets);
+
+    // console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push(`pluto`)
+    console.log(planets);
+
+    // console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift()
+    console.log(planets);
+
+    // console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop()
+    console.log(planets);
+
+    // console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf(`Earth`))
+
+    // console.log("Reversing the order of the planets array.");
+planets.reverse();
+    console.log(planets);
+
+   // console.log("Sorting the planets array.");
+    planets.sort();
+    console.log(planets);
+})();
+
+
+(function(){
+    "use strict";
+
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
     var planetsArray;
 
@@ -10,6 +59,7 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
+
     planetsArray=planetsString.split(`|`)
      console.log(planetsArray);
 
@@ -25,10 +75,7 @@
      */
     let planetsString2=planetsArray.join(`<br>`)
     console.log(planetsString2);
-    // function function1(ul) {
-    //     ul.innerHTML += `<li> four </li>`;
-    // }
-    // console.log(function1(`beans`))
-
-
+    document.write(planetsString2);
+   let planetsList=`<ul><li/>` + planetsArray.join(`</>li<li>`) + <li/><ul>';
 })();
+
