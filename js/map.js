d3cf36209b4811fe08a861af31d96c8124153178
Reginald Graham <reg.graham98@gmail.com>
2023-01-26 09:32:30 -0600

    working on function-drills

--- js/map.js
+++ js/map.js
@@ -9,7 +9,6 @@ findButton.addEventListener(`click`, function(){
 
     let city = document.querySelector("#location-input").value;
 
-    console.log(locationInput.value, 'this is input value')
     geocode(locationInput.value, keys.mapbox).then(function(result){
         map.setCenter(result);
         map.setZoom(9);
@@ -18,18 +17,13 @@ findButton.addEventListener(`click`, function(){
             .addTo(map);
     });
 
-    console.log(city, "this is city var")
     let html = '';
     $.get("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+keys.weather+"",
         {
             units: 'imperial'
         }).done(function(data) {
         for (let i = 0; i < data.list.length; i += 8) {
-            console.log(data.list[i], 'this is data.list');
             let temp = data.list[i];
-            console.log(data.list[i].main.temp, 'this is data.list');
-            console.log(temp)
-            console.log(data, 'this is temp');
             html += `<div class="row2">
     <div>${data.list[i].dt_txt} <br><hr></div>
     <div>temp: ${data.list[i].main.temp}<br><hr></div>
@@ -48,7 +42,6 @@ findButton.addEventListener(`click`, function(){
     let htm='';
     htm += `<div id="city-log"> <h3> Current city: ${city}</h3> </div>`;
     $(`#city-log`).html(htm);
-
-
 });
+
 
